e9f9dc172c4df58d559a31e6415934dae122a10c
doeksul <46569112+doeksul@users.noreply.github.com>
2023-06-27 18:43:12 +0300

    tema1 siso

---
+++ tema1/siso/design.sv
@@ -0,0 +1,32 @@
+// Code your design here
+// Code your design here
+
+//PIPO
+
+
+module bistabil_D(input a,input clk,output reg O);
+ 
+  always @(posedge clk)
+    begin
+      O<=a;
+    end
+endmodule
+/*
+module registru(input[3:0] a,input clk, output [3:0] out);
+  
+  bistabil_D b1(a,clk,out);
+  bistabil_D b2(a[2],clk,out[2]);
+  bistabil_D b3(a[1],clk,out[1]);
+  bistabil_D b4(a[0],clk,out[0]);
+endmodule
+*/
+module registru2(input a,input clk, output  out);
+  
+  wire out1,out2,out3;
+  bistabil_D b1(a,clk,out1);
+  bistabil_D b2(out1,clk,out2);
+  bistabil_D b3(out2,clk,out3);
+  bistabil_D b4(out3,clk,out);
+   
+ 
+endmodule
---
+++ tema1/siso/testbench.sv
@@ -0,0 +1,32 @@
+// Code your testbench here
+// or browse Examples
+// Code your testbench here
+// or browse Examples
+
+
+module testbench();
+  
+  reg clk;
+  reg a;
+  wire O;
+ 
+  registru2 Ichip1(a,clk,O);
+  
+  
+  
+  
+  initial clk= 1'b0;
+  initial a=1'b1;
+  
+  initial forever #250 clk = ~clk;
+	initial forever #1000 a=~a;
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+   
+    #9000;
+    $finish(1);
+  end
+  
+  
+endmodule
